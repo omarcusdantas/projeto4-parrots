684485a5b69b2d11bca6eb534109611fef2a2999
MarcusDantas <marvinsd@gmail.com>
2023-04-08 11:53:13 -0300

    game logic introduced

--- src/script.js
+++ src/script.js
@@ -1,3 +1,9 @@
+let pointsNeeded = 0;
+let turns = 0;
+let points = 0;
+let firstCard = null;
+let secondCard = null;
+
 startGame();
 
 function startGame() {
@@ -6,6 +12,7 @@ function startGame() {
         numberCards = prompt('Com quantas cartas quer jogar? Escolha um número par entre 4 e 14');
     }
     shuffleImages(numberCards);
+    pointsNeeded = numberCards/2;
 }
 
 function shuffleImages(num) {
@@ -31,11 +38,50 @@ function renderCards(cardImages) {
     const cardsList = document.querySelector('.cards-container');
     cardsList.innerHTML = '';
     for (let i = 0; i < cardImages.length; i++) {
-        cardsList.innerHTML += `<li class="card" onclick="flipCard(this)"><img class="back-face" src="./src/img/back.png" alt="back of the card">` +
+        cardsList.innerHTML += `<li class="card" onclick="manageGame(this)"><img class="back-face" src="./src/img/back.png" alt="back of the card">` +
         `<img class="front-face" src="./src/img/${cardImages[i]}" alt="front of the card"></li>`;
     }
 }
 
 function flipCard(card) {
     card.classList.toggle('flip');
+}
+
+function manageGame(card) {
+    if (firstCard != null && secondCard != null) {
+        return;
+    }
+    else if (firstCard === null) {
+        flipCard(card);
+        firstCard = card;
+        turns++;
+        return;
+    }
+    else if (firstCard === card) {
+        return;
+    }
+    flipCard(card);
+    secondCard = card;
+    turns ++;
+    setTimeout(compareCards,1000);         
+}
+
+function compareCards() {
+    if (firstCard.innerHTML === secondCard.innerHTML) {
+        firstCard = null;
+        secondCard = null;
+        managePoints();
+        return;
+    }
+    setTimeout(flipCard,1000, firstCard);
+    setTimeout(flipCard,1000, secondCard);
+    setTimeout(() => firstCard = null, 1000);
+    setTimeout(() => secondCard = null, 1000);
+}
+
+function managePoints(){
+    points++;
+    if (points === pointsNeeded){
+        alert(`Você ganhou em ${turns} jogadas!`);
+    }
 }
