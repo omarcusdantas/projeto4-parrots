bc66036482a1abe0c1f63ab3e1e9f22d42ba6138
MarcusDantas <marvinsd@gmail.com>
2023-04-08 12:05:55 -0300

    changed renderCards function

--- src/script.js
+++ src/script.js
@@ -38,9 +38,9 @@ function renderCards(cardImages) {
     const cardsList = document.querySelector('.cards-container');
     cardsList.innerHTML = '';
     for (let i = 0; i < cardImages.length; i++) {
-        cardsList.innerHTML += `<li class="card" data-test="card" onclick="manageGame(this)">
-        <img class="back-face" data-test="face-down-image" src="./src/img/back.png" alt="back of the card">
-        <img class="front-face"  data-test="face-up-image" src="./src/img/${cardImages[i]}" alt="front of the card"></li>`;
+        cardsList.innerHTML += `<li data-test="card" class="card" onclick="manageGame(this)">`+
+        `<img data-test="face-down-image" class="back-face" src="./src/img/back.png" alt="back of the card">`+
+        `<img data-test="face-up-image" class="front-face" src="./src/img/${cardImages[i]}" alt="front of the card"></li>`;
     }
 }
 
